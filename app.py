010d0cb3d150452af258301d22d8e10a91f943c7
IOANNIS <tzavaras95@gmail.com>
2020-05-10 17:15:53 +0300

    nothing

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+<<<<<<< HEAD
 st.title("Risk Analysis in Cancer !")
 st.markdown("## Please fill the infos and then press submit !")
 
@@ -9,21 +10,39 @@ st.markdown("## Please fill the infos and then press submit !")
 age = st.selectbox(
     'Age',
     ('0', '1', '2',"3","4","5")
+=======
+st.title("Application for Risk Analysis")
+st.markdown("## Please fill the following fields and press submit to calculate the possibility of breast cancer reappear")
+
+
+age = st.selectbox(
+    'age',
+    ('0 "20-29" ', '1 "30-39" ', '2 "40-49" ','3 "50-59" ','4 "60-69" ','5 "70-79" ')
+>>>>>>> 1129b97c4022de91ed40ad5ec629ee14f83b6290
 )
 # st.text(age)
 
 
 menopause = st.selectbox(
     'menopause',
+<<<<<<< HEAD
     ('0', '1', '2')
 )
 tumor_size = st.selectbox(
     'tumor-size',
     ('0', '1', '2',"3","4","5","6","7","8","9","10")
+=======
+    ('0 "premeno" ', '1 "ge40" ', '2 "lt40" ')
+)
+tumor_size = st.selectbox(
+    'tumor-size',
+    ('0 "0-4" ', '1 "5-9" ', '2 "10-14" ','3 "15-19" ','4 "20-24" ','5 "25-29" ','6 "30-34" ','7 "35-39" ','8 "40-44" ','9 "45-49" ','10 "50-54" ')
+>>>>>>> 1129b97c4022de91ed40ad5ec629ee14f83b6290
 )
 
 inv_nodes = st.selectbox(
     'inv-nodes',
+<<<<<<< HEAD
     ('0', '1', '2',"3","4","5","6")
 )
 node_caps = st.selectbox(
@@ -41,11 +60,34 @@ breast = st.selectbox(
 breast_quad = st.selectbox(
     'breast-quad',
     ('0 "dsdsd"', '1', '2',"3","4","200")
+=======
+    ('0 "0-2" ', '1 "3-5" ', '2 6-8 ','3 "9-11" ','4 "12-14" ','5 "15-17" ','6 "24-26" ')
+)
+node_caps = st.selectbox(
+    'node-caps',
+    ('0 "no"','1 "yes"')
+)
+deg_malig = st.selectbox(
+    'deg-malig',
+    ('1','2','3')
+)
+breast = st.selectbox(
+    'breast',
+    ('0 "left" ','1 "right"')
+)
+breast_quad = st.selectbox(
+    'breast-quad',
+    ('0 "left_low"', '1 "right_up"', '2 "left-up"','3 "right-low"','4 "central"')
+>>>>>>> 1129b97c4022de91ed40ad5ec629ee14f83b6290
 )
 
 
 
 def svm(age, menopause, tumor_size , inv_nodes ,node_caps, deg_malig , breast,breast_quad):
+<<<<<<< HEAD
+=======
+
+>>>>>>> 1129b97c4022de91ed40ad5ec629ee14f83b6290
     # Import train_test_split function
     from sklearn.model_selection import train_test_split
 
@@ -76,6 +118,7 @@ def svm(age, menopause, tumor_size , inv_nodes ,node_caps, deg_malig , breast,br
 
     #Predict the response for test dataset
     y_pred = clf.predict(X_test)
+<<<<<<< HEAD
     st.text(y_pred)
 
 
@@ -84,3 +127,18 @@ if st.button("Submit"):
 
 
 st.text("dwedewdewfdefesd")  
+=======
+
+    if y_pred == 0:
+        result = "Most possible is not to reappear with the possibility of error 25%"
+    elif y_pred == 1:
+        result = "Most possible is reappear with the possibility of error 25%"
+
+    st.text(result)
+
+
+if st.button("Submit"):
+    svm(age[0], menopause[0], tumor_size[0], inv_nodes[0], node_caps[0], deg_malig[0], breast[0], breast_quad[0])
+
+
+>>>>>>> 1129b97c4022de91ed40ad5ec629ee14f83b6290
